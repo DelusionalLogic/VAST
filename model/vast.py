4c5883c9d2df01534f5d62a39ecd0b8a068e1876
Jesper Jensen <jesper@jnsn.dev>
2024-05-15 23:33:57 +0200

    Remove unused params

--- model/vast.py
+++ model/vast.py
@@ -48,8 +48,7 @@ to_2tuple = _ntuple(2)
 
 class Attention(nn.Module):
     def __init__(
-            self, dim, num_heads=8, qkv_bias=True, qk_scale=None,
-            proj_drop=0., norm_layer=nn.LayerNorm):
+            self, dim, num_heads=8, qk_scale=None):
         super().__init__()
         self.num_heads = num_heads
         head_dim = dim // num_heads
@@ -94,13 +93,10 @@ class Attention(nn.Module):
         return x
 
 class Block(nn.Module):
-    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=True, qk_scale=None, drop=0.,
-                 drop_path=0., init_values=None, norm_layer=nn.LayerNorm, postnorm=False):
+    def __init__(self, dim, num_heads, mlp_ratio=4., qk_scale=None, init_values=None, norm_layer=nn.LayerNorm, postnorm=False):
         super().__init__()
         self.norm1 = norm_layer(dim)
-        self.attn = Attention(
-            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, proj_drop=drop,
-            norm_layer=norm_layer)
+        self.attn = Attention(dim, num_heads=num_heads, qk_scale=qk_scale)
         # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
@@ -165,11 +161,10 @@ class EVAVisionTransformer(nn.Module):
     """ Vision Transformer with support for patch or hybrid CNN input stage
     """
     def __init__(self, img_size, patch_size=16, in_chans=3, embed_dim=768, depth=12,
-                 num_heads=12, mlp_ratio=4., qkv_bias=True, qk_scale=None,
-                 drop_path_rate=0.4, norm_layer=nn.LayerNorm, init_values=None,
+                 num_heads=12, mlp_ratio=4., qk_scale=None,
+                 norm_layer=nn.LayerNorm, init_values=None,
                  use_abs_pos_emb=True,
                  init_scale=0.001, postnorm=False):
-        drop_rate = 0
         super().__init__()
         self.image_size = img_size
         self.num_classes = 1024
@@ -183,14 +178,10 @@ class EVAVisionTransformer(nn.Module):
         assert use_abs_pos_emb
         self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, embed_dim))
 
-        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  # stochastic depth decay rule
         self.blocks = nn.ModuleList([
             Block(
-                dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-                drop=drop_rate, drop_path=dpr[i], norm_layer=norm_layer,
-                init_values=init_values,
-                postnorm=postnorm)
-            for i in range(depth)])
+                dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qk_scale=qk_scale, norm_layer=norm_layer, init_values=init_values, postnorm=postnorm)
+            for _ in range(depth)])
         self.norm = norm_layer(embed_dim)
         self.fc_norm = None
         self.head = nn.Linear(embed_dim, self.num_classes)
@@ -228,14 +219,11 @@ class CustomCLIP(nn.Module):
         self.visual = EVAVisionTransformer(
             img_size=image_size,
             patch_size=14,
-            use_mean_pooling=False,
             init_values=None,
             embed_dim=1408,
             depth=40,
             num_heads=16,
             mlp_ratio=4.3637,
-            qkv_bias=True,
-            drop_path_rate=0.4,
             norm_layer=partial(nn.LayerNorm, eps=1e-6),
             postnorm=False
         )
